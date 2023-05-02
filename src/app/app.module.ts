362623cd220e01c7b3005376dd8f0209d2f4f0dd
mhd329 <mhd329@gmail.com>
2023-05-02 23:48:10 +0900

    2023 5 2 angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+
+import { RouterModule, Routes } from "@angular/router";
+
+const router: Routes = [
+  { path: "login", component: LoginComponent },
+  { path: "todo-list", component: TodoListComponent },
+  { path: '', redirectTo: "/login", pathMatch: "full" },
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(router, { enableTracing: false }),
   ],
   providers: [],
   bootstrap: [AppComponent]
